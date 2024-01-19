a31866a640f8e567a3c18224c838a672d37fdc8a
Saori Nishimitsu <seelight.saori@gmail.com>
2024-01-19 15:21:11 +0900

    共通ヘッダー・フッター作成

---
+++ resources/views/layouts/announce-header.blade.php
@@ -0,0 +1,17 @@
+<section class="flex bg-white shadow h-10 py-2 border-b-2">
+  <div class="container mx-auto flex justify-between">
+    <p class="">今日も料理が楽しみだ</p>
+    <div class="flex">
+      {{-- ログインをしていたら表示 --}}
+      @auth
+        <a href="{{ route('profile.edit') }}" class="ml-auto">マイページ</a>
+      @endauth
+
+      {{-- 未ログイン --}}
+      @guest
+        <a href="{{ route('register') }}" class="mr-2">ユーザー登録(無料)</a>
+        <a href="{{ route('login') }}" class="">ログイン</a>
+      @endguest
+    </div>
+  </div>
+</section>

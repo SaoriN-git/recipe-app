fe70c970d131f071f7211a1e7d805180ca61e1be
SaoriNishimitsu <129496603+SaoriNishimitsu@users.noreply.github.com>
2024-01-28 18:42:59 +0900

    詳細ページ表示、レイアウト調整

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -88,10 +88,9 @@ class RecipeController extends Controller
    */
   public function show(string $id)
   {
-    $recipe = Recipe::with(['ingredients', 'steps', 'reviews'])
+    $recipe = Recipe::with(['ingredients', 'steps', 'reviews.user', 'user'])
       ->where('recipes.id', $id)
-      ->get();
-      $recipe = $recipe[0];
+      ->first();
       $recipe_recorde = Recipe::find($id);
       $recipe_recorde->increment('views');
     //リレーションで材料とステップを取得
